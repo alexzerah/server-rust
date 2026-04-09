2029767cf4917ebf8f0530b8e28c566227c6e5a8
Alexandre Zerah <alex.zerah@gmail.com>
2026-04-09 19:28:24 +0200

    ok

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,37 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Server running on http://127.0.0.1:8080");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let body = "<!DOCTYPE html>
+    <html>
+    <head><title>Hello</title></head>
+    <body>
+    <h1>Hello, World!</h1>
+    <p>Bienvenue sur mon serveur Rust </p>
+    </body>
+    </html>";
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: text/html\r\n\r\n{}",
+        body.len(),
+        body
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+    println!("Response sent to client");
 }
